bfd0aff7c0597aac5fbbf7818d2cfbf2157d1b11
nguyenbinh09 <21521878@gm.uit.edu.vn>
2025-01-10 17:17:39 +0700

    add more time for refreshToken

--- src/main/java/com/example/demo/services/JWTService.java
+++ src/main/java/com/example/demo/services/JWTService.java
@@ -10,7 +10,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
-import java.nio.charset.StandardCharsets;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.function.Function;
@@ -45,7 +44,7 @@ public class JWTService {
     }
 
     public String createRefreshToken(HashMap<String, Object> extraClaims, UserDetails userDetails) {
-        int refreshTokenExpiration = 604800;
+        int refreshTokenExpiration = 604800000;
         return buildToken(extraClaims, userDetails, refreshTokenExpiration);
     }
 
